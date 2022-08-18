c43ba9f68f9b9e3d448b8943d33f5908dabe5941
FlavioVillar <flavio_villar@hotmail.com>
2022-08-18 19:05:17 -0300

    test: :white_check_mark: criado testes para a rota de login
    testes de validação da rota login

--- app/backend/src/middlewares/error.middleware.ts
+++ app/backend/src/middlewares/error.middleware.ts
@@ -4,11 +4,11 @@ import HttpException from '../validation/HttpException';
 
 class ErrorMiddleware {
   errorHandler: ErrorRequestHandler =
-  (err: HttpException, _req: Request, res: Response, _next: NextFunction) => {
+  (err: Error, _req: Request, res: Response, _next: NextFunction) => {
     if (err instanceof HttpException) {
       res.status(err.statusCode).json({ message: err.message });
     } else {
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: err.message });
     }
   };
 }
@@ -16,25 +16,3 @@ class ErrorMiddleware {
 const errorMiddleware = new ErrorMiddleware();
 
 export default errorMiddleware;
-
-// const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
-//   const { name, message } = err;
-//   switch (name) {
-//     case 'ValidationError':
-//       return res.status(400).json({ message });
-//     case 'JsonWebTokenError':
-//       return res.status(401).json({ message });
-//     case 'NotFoundError':
-//       return res.status(404).json({ message });
-//     case 'ConflictError':
-//       return res.status(409).json({ message });
-//     case 'UnauthorizedError':
-//       res.status(401).json({ message });
-//       break;
-//     default:
-//       res.status(500).json({ message });
-//       break;
-//   }
-// };
-
-// export default errorMiddleware;
--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,51 +1,81 @@
-import * as sinon from 'sinon';
+import * as Sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
-
 import { app } from '../app';
 import Example from '../database/models/ExampleModel';
-
 import { Response } from 'superagent';
 import JwtService from '../services/jwt.service';
-import ICreateUser from '../interfaces/ICreateUser.interface';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-// const createUserLoginMock: ICreateUser = {
-//   id: 1,
-//   email: 'any-email'
+const createUserLoginMock = {
+  email: 'admin@admin.com' as string,
+  password: 'scret_admin' as string,
+}
+
+const createUserLoginMockInvalid = {
+  email: 'test@test.com' as string,
+  password: 'test_hash' as string,
+}
 
 
-// describe('Teste Login', () => {
-//   beforeEach(() => {
-//     Sinon.stub(JwtService, 'sign').returns('token');
 
-  // let chaiHttpResponse: Response;
+describe('Login', () => {
+  beforeEach(() => {
+    Sinon.stub(JwtService, 'createToken').returns('any-token');
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
+  });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+  afterEach(() => {
+    Sinon.restore();
+  })
+    
+  it('Deve retornar status 200', async () => {
+    const response = await chai.request(app)
+      .post('/login')
+      .send(createUserLoginMock);
+      
+    expect(response.status).to.be.eq(200);
+  });
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
+  it('Deve retornar um token', async () => {
+    const response = await chai.request(app)
+      .post('/login')
+      .send(createUserLoginMock);
 
-  //   expect(...)
-  // });
+    expect(response.body).to.be.deep.eq({ token: 'any-token' });
+  });
+  
+  it('Deve retornar status 400', async () => {
+    const response = await chai.request(app)
+      .post('/login')
+      .send({});
+    
+    expect(response.status).to.be.eq(400);
+  });
 
-//   it('Seu sub-teste', () => {
-//     expect(false).to.be.eq(true);
-//   });
-// });
+  it('Deve retornar message de erro "All fields must be filled"', async () => {
+    const response = await chai.request(app)
+      .post('/login')
+      .send({});
+    
+    expect(response.body).to.be.deep.eq({ message: 'All fields must be filled' });
+  });
+
+  it('Deve retornar message de erro "Incorrect email or password"', async () => {
+    const response = await chai.request(app)
+      .post('/login')
+      .send(createUserLoginMockInvalid);
+    
+    expect(response.body).to.be.deep.eq({ message: 'Incorrect email or password' });
+  });
+});
+
+
+
+
+
+
